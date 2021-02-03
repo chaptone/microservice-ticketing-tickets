b1ac2ad827942e132f383914199850be76f06c32
chaptone <rakkan.j@gmail.com>
2021-02-03 10:30:40 +0700

    Added: tickets CRUD with test!

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@rkktickets/common";
 import { createTicketRoute } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -17,6 +20,9 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRoute);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
